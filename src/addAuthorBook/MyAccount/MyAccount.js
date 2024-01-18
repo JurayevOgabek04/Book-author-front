a8a37b672d4c6db7460ffcb06972be414d28b6e0
ogabek <jurayev.ogabek0523@gmail.com>
2024-01-18 23:34:37 +0500

    fixed axios account

--- src/addAuthorBook/MyAccount/MyAccount.js
+++ src/addAuthorBook/MyAccount/MyAccount.js
@@ -1,41 +1,45 @@
 import axios from "axios"
-import { useRef, useContext, useState, useEffect } from "react"
+import { useRef, useContext, useEffect, useState } from "react"
 import { AuthContext } from "../../context/Authcontext"
 import "./myaccount.css"
 
 
 
 export const MyAccount = () => {
-
     const token = useContext(AuthContext)
     const [info, setInfo] = useState([])
 
     const elFirst = useRef("")
     const elLast = useRef("")
     const elNumber = useRef("")
-    const elPhoto = useRef("")
+    // const elPhoto = useRef("")
 
     useEffect(() => {
-        axios.get("https://book-service-layer.herokuapp.com/user/me", {
+        axios.get("http://localhost:3000/user/me", {
             headers: {
                 Authorization: token.token
             }
         })
             .then(res => setInfo(res.data))
-    }, [elFirst, elLast, elNumber, elPhoto])
+    }, [elFirst, elLast, elNumber])
 
 
 
     const handelFormAcc = (evt) => {
         evt.preventDefault()
-        const formAcc = new FormData()
 
-        formAcc.append("first_name", elFirst.current.value);
-        formAcc.append("last_name", elLast.current.value);
-        formAcc.append("phone", elNumber.current.value);
-        formAcc.append("image", elPhoto.current.files[0]);
+        const formAcc = {
+            first_name: elFirst.current.value,
+            last_name: elLast.current.value,
+            phone: elNumber.current.value,
+        }
+        // const formAcc = new FormData()
+        // formAcc.append("first_name", elFirst.current.value);
+        // formAcc.append("last_name", elLast.current.value);
+        // formAcc.append("phone", elNumber.current.value);
+        // formAcc.append("image", elPhoto.current.files[0]);
 
-        axios.put("https://book-service-layer.herokuapp.com/user/account", formAcc, {
+        axios.patch("http://localhost:3000/user/account", formAcc, {
             headers: {
                 Authorization: token.token
             }
@@ -57,20 +61,20 @@ export const MyAccount = () => {
 
                     <div>
                         <h4 className="input__title">First Name</h4>
-                        <input ref={elFirst} type="text" className="form-control" defaultValue={info.first_name} required />
+                        <input ref={elFirst} type="text" className="form-control" defaultValue={info.firstname} />
                         <p className="input__text">Please enter your first name.</p>
                     </div>
                     <div>
                         <h4 className="input__title">Last Name</h4>
-                        <input ref={elLast} type="text" className="form-control" defaultValue={info.last_name} required />
+                        <input ref={elLast} type="text" className="form-control" defaultValue={info.lastname} />
                         <p className="input__text">Please enter your last name.</p>
                     </div>
                     <div>
                         <h4 className="input__title">Phone</h4>
-                        <input ref={elNumber} type="tel" className="form-control" defaultValue={info.phone} required />
+                        <input ref={elNumber} type="tel" className="form-control" defaultValue={info.phone} />
                         <p className="input__text">Please enter your  phone number.</p>
                     </div>
-                    <input ref={elPhoto} type="file" className="form-control " required />
+                    {/* <input ref={elPhoto} type="file" className="form-control " required /> */}
 
                     <button className="save__btn" type="submit">Save Changes</button>
 
